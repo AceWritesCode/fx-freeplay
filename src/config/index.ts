afe2e984525e75a9783cb28508a689a057a4a4ec
Himanshu <hacker.ht.ht47@gmail.com>
2026-09-20 14:09:55 +0530

    chore(release): bump version to v1.1.0

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,5 +2,5 @@ export * from './timeframes';
 export * from './timezones';
 export * from './chartPresets';
 export * from './themes';
-export const APP_VERSION = '1.0.0';
+export const APP_VERSION = '1.1.0';
 export const MAX_CANDLE_ROWS = 300000;
